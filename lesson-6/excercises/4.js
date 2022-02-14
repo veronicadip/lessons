6e6f5b0ce8d930fd42ced0dc79e8cdd83aaf1237
veronicadip <dipmveronica@gmail.com>
2022-02-14 13:04:04 -0300

    js4 finished

--- lesson-6/excercises/4.js
+++ lesson-6/excercises/4.js
@@ -4,9 +4,16 @@
   */
 
 const result1 = true && true
+true
 const result2 = true || false
+true
 const result3 = true && true || false
+true
 const result4 = true && 'string'
+"string"
 const result5 = false || { an: 'object' } 
+"object"
 const result6 = true || 12
+true
 const result7 = [] || true
+true
