48b77d5d05ae6d9f8f3a8f504c64aa104291a3a9
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-02-14 16:07:57 -0300

    js7 finished (#11)
    js7 checked

--- lesson-6/excercises/7.js
+++ lesson-6/excercises/7.js
@@ -2,11 +2,14 @@
   * If you run the following code it will give you an error.
   *
   * Why? Investigate what you can change in the code to avoid that error
-  *
+  * 
+  * You can't define another variable
+  * 
   */
 
-const myVariable = "yes!"
+let myVariable = "yes!"
 
 myVariable = 1
 
 console.table({ myVariable })
+
