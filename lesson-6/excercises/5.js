4241ebdab8d058fd23589bed411b6e5d3dbc063f
veronicadip <dipmveronica@gmail.com>
2022-02-14 14:36:55 -0300

    js5 finished

--- lesson-6/excercises/5.js
+++ lesson-6/excercises/5.js
@@ -6,3 +6,10 @@
   * The last element is {LAST ELEMENT HERE}
   *
   */
+const animals = "This array has 5 elements. The first element is cat. The last element is bird";
+const firstelement = "cat";
+const secondelement = "dog";
+const thirdelement = "parrot";
+const fourthelement = "frog";
+const lastelement = "bird";
+const array = ["cat", "dog", "parrot", "frog", "bird"];
