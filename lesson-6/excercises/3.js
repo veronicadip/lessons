1abf5a924809fe5948124593d566761cbec652b4
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-02-14 15:09:44 -0300

    js3 finished (#7)
    js3 checked

--- lesson-6/excercises/3.js
+++ lesson-6/excercises/3.js
@@ -4,3 +4,8 @@
   * - Three different variables
   * - An object that has keys with the variables
   */
+
+const firstname = "vero";
+const lastname = "dip";
+const age = 21;
+const me = {firstname: firstname, lastname: lastname, age: age}
