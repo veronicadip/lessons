e60975976799be7fec0a4f948a9f7683ec955a40
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-02-14 15:21:16 -0300

    js5 finished (#9)
    js5 checked

--- lesson-6/excercises/5.js
+++ lesson-6/excercises/5.js
@@ -6,3 +6,13 @@
   * The last element is {LAST ELEMENT HERE}
   *
   */
+const firstelement = "cat";
+const secondelement = "dog";
+const thirdelement = "parrot";
+const fourthelement = "frog";
+const lastelement = "bird";
+const array1 = [firstelement, secondelement, thirdelement, fourthelement, lastelement];
+
+console.log("This array has " + (array1.length) + " elements")
+console.log("The first element is " + array1[0])
+console.log("The last element is " + array1[array1.length -1])
