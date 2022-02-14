7e3ee79cb7b0bff132535152a95618f5da71fab8
veronicadip <dipmveronica@gmail.com>
2022-02-14 11:12:58 -0300

    js1 ready to be checked

--- lesson-6/excercises/1.js
+++ lesson-6/excercises/1.js
@@ -9,3 +9,17 @@
   * - A variable bool that is a boolean
   * - A variable obj that is an object with 4 properties
   */
+
+const str = "vero";
+
+const number = 1;
+
+const float = 1.5;
+
+const dogs = ["puki", "kira", "tommy"];
+
+const things = ["pencil", 1,5, "stars", 3, "puki", 1];
+
+const bool = true;
+
+const obj = {name:"tommy", age:"10", color:"grey", animal:"dog"}
