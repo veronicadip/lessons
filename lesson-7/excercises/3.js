e36ad203bd6e5e9cd469a2deffbe8cb27a283a45
veronicadip <dipmveronica@gmail.com>
2022-02-17 16:45:48 -0300

    lesson-7 finally finished :)

--- lesson-7/excercises/3.js
+++ lesson-7/excercises/3.js
@@ -4,36 +4,65 @@
  * Simulate wordle as played in a terminal
  */
 
-const words = ["store", "plant", "green"];
+const levels = ["store", "plant", "green"];
+const wordLength = 5;
+const wrongLetter = "_";
 
-function wordle(word, day) {
-  if (words >= length[6]) {
+function wordle(guess, currentLevel) {
+  let answer = "";
+  const currentWord = levels[currentLevel]
+
+  if (currentLevel >= levels.length) {
+  return "That level doesn't exist"
+  }
+
+  if (guess.length !== wordLength) {
     return false;
-  } else {
+  }
+
+  if (guess === currentWord) {
     return true;
   }
+
+  for (let number = 0; number <= 4; number++) {
+    if (guess.at(number) !== currentWord.at(number)) {
+      answer = answer + wrongLetter;
+    } else {
+      answer = answer + currentWord.at(number);
+    }
+  }
+
+  return answer;
 }
 
-if (wordle("longword") !== false) {
+if (wordle("longword", 0) !== false) {
   throw new Error(
     "It should return false when the word is longer than 5 letters"
   );
 }
 
-if (wordle("shot") !== false) {
+if (wordle("shot", 0) !== false) {
   throw new Error(
     "It should return false when the word is shorter than 5 letters"
   );
 }
 
-if (wordle("store", 1) !== true) {
+if (wordle("store", 0) !== true) {
   throw new Error("It should return true when the word of the day is correct");
 }
 
-if (wordle("sture") !== "st_re") {
+if (wordle("sture", 0) !== "st_re") {
   throw new Error(
     "It should show the correct letters and use underscores for the wrong letters"
   );
+}
+
+if (wordle("plant", 1) !== true) {
+  throw new Error("It should return true when the word of the day is correct");
+}
+
+if (wordle("", 89) !== "That level doesn't exist") {
+  throw new Error("It should return an error when the level doesn't exist")
 }
 
 console.log("CONGRATS! ALL TESTS HAVE PASSED");
