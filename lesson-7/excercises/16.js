f0636b08096b5c095fe753636a6528f10d09b856
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-02-22 19:19:15 -0300

    js16 checked (#21)
    js16 done

--- lesson-7/excercises/16.js
+++ lesson-7/excercises/16.js
@@ -5,3 +5,21 @@
  * Example: flatten([[1, 2], [3, 4]]);
 // => [1, 2, 3, 4]
  */
+
+function flatten(containerArr) {
+  let result = [];
+  for (let counter1 = 0; counter1 !== containerArr.length; counter1++) {
+    let array = containerArr.at(counter1);
+    for (let counter2 = 0; counter2 !== array.length; counter2++) {
+      result.push(array.at(counter2));
+    }
+  }
+  return result;
+}
+
+console.log(
+  flatten([
+    [1, 2],
+    [3, 4],
+  ])
+);
