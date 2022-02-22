8a8b6e683006c63e4de7bd225f7e0687340163d1
veronicadip <dipmveronica@gmail.com>
2022-02-22 19:17:36 -0300

    js17 ready to be checked :)

--- lesson-7/excercises/17.js
+++ lesson-7/excercises/17.js
@@ -1,7 +1,17 @@
 /**
  * Write a function that takes two numbers n1 and n2 and returns an array
  * in which all elements are n1 and has length n2
- * 
+ *
  * Example: fill(10, 3)
  * returns [10, 10, 10]
  */
+
+function fill(n1, n2) {
+  result = [];
+  for (let counter = 0; counter !== n2; counter++) {
+    result.push(n1);
+  }
+  return result;
+}
+
+console.log(fill(10, 3));
