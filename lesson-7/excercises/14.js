67b23111cc17a349a2f1a0f602bc2752b0d800ff
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-02-22 19:18:55 -0300

    js14 checked (#19)
    js14 done

--- lesson-7/excercises/14.js
+++ lesson-7/excercises/14.js
@@ -2,7 +2,18 @@
  * Write a function that takes two arrays and returns
  * a single array that contains the elements of both
  * arrays alternated
- * 
+ *
  * example: combineArr(['a', 'b'], [1, 2])
  * returns ['a', 1, 'b', 2]
  */
+
+function combineArr(array1, array2) {
+  let result = [];
+  for (let counter = 0; counter !== array1.length; counter++) {
+    result.push(array1.at(counter));
+    result.push(array2.at(counter));
+  }
+  return result;
+}
+
+console.log(combineArr(["a", "b"], [1, 2]));
