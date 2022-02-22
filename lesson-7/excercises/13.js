c24cce87814c9bbb06d123799eb8e022c518ec9d
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-02-22 18:52:38 -0300

    js13 checked (#18)
    js13 done

--- lesson-7/excercises/13.js
+++ lesson-7/excercises/13.js
@@ -1,7 +1,13 @@
 /**
  * Write a function that receives three parameters: name, age and address
  * and returns an object with the values as the keys
- * 
+ *
  * example: buildPerson('Puki', 1, 'Santa Fe 940')
  * returns { name: 'Puki', age: 1, address: 'Santa Fe 940' }
  */
+
+function buildPerson(name, age, address) {
+  return { name, age, address };
+}
+
+console.log(buildPerson("Puki", 1, "Santa Fe 940"));
