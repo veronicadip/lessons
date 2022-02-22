5dc9435f2bc98bfc120df5bb70fd14befc0b11af
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-02-22 11:01:15 -0300

    js10 checked

--- lesson-7/excercises/10.js
+++ lesson-7/excercises/10.js
@@ -8,3 +8,13 @@
  * 
  * should return [3, 5]
  */
+
+function sumArrays(array1, array2) {
+    let result = []
+    for (let counter = 0; counter !== array1.length; counter++) {
+        result.push(array1.at(counter) + array2.at(counter))
+    }
+    return result
+}
+
+console.log(sumArrays([1, 2], [2, 3]))
