c6506fe82092875dd61624e04b18f6b3612e64d9
veronicadip <dipmveronica@gmail.com>
2022-02-22 15:02:33 -0300

    js12 ready to be checked

--- lesson-7/excercises/12.js
+++ lesson-7/excercises/12.js
@@ -1,6 +1,16 @@
 /**
  * Define a function that receives a number as an argument and returns
  * the sum of all numbers up until the provided number.
- * 
+ *
  * Example: sumTo(5) returns 15 because 1 + 2 + 3 + 4 + 5 = 15
  */
+
+function sumTo(number) {
+  let result = 0;
+  for (let counter = 0; counter !== number + 1; counter++) {
+    result = result + counter;
+  }
+  return result;
+}
+
+console.log(sumTo(5))
