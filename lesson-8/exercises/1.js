ff31200f12cad110df1f9ba4ff3c000669a4251a
veronicadip <dipmveronica@gmail.com>
2022-02-23 13:06:00 -0300

    js1 ready to be checked

--- lesson-8/exercises/1.js
+++ lesson-8/exercises/1.js
@@ -1,7 +1,14 @@
 /**
  * Write your own version of Array.map that receives an
  * array and a function and applies the function to each item
- * 
+ *
  *  example: myMap([1, 2, 3], function (item) { return item + 1 })
  *  returns [2, 3, 4]
  */
+
+const array = [5, 10, 15];
+const multiplyArr = array.map(function (item) {
+  return item * 5;
+});
+
+console.log(multiplyArr);
