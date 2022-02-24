d1cb4d5316f4f0c7dfb3050772777b89147f7962
veronicadip <dipmveronica@gmail.com>
2022-02-24 16:35:14 -0300

    js4 ready to be checked

--- lesson-8/exercises/4.js
+++ lesson-8/exercises/4.js
@@ -5,3 +5,44 @@
  * classes would you create and what attributes could they have?
  *
  */
+
+class Person {
+  constructor(personName, personAge) {
+    this.name = personName;
+    this.age = personAge;
+  }
+  presentation() {
+    console.log("Hi, my name is " + this.name + " and my age is " + this.age);
+  }
+}
+
+class Professor extends Person {}
+
+const Fulano = new Professor("Fulano", 37);
+{
+  Fulano.presentation();
+}
+
+const Mengano = new Professor("Mengano", 49);
+{
+  Mengano.presentation();
+}
+
+class Student extends Person {}
+
+const Fulanito = new Student("Fulanito", 14);
+{
+  Fulanito.presentation();
+}
+
+const Menganito = new Student("Menganito", 16);
+{
+  Menganito.presentation();
+}
+
+class Lesson {
+  constructor(lessonSubject, lessonNumber) {
+    this.subject = lessonSubject;
+    this.number = lessonNumber;
+  }
+}
