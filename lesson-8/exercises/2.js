b9332b2b60684435bc1462bf73ef6064a2c52dd9
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-02-24 16:36:46 -0300

    js2 checked (#24)
    js2 done

--- lesson-8/exercises/2.js
+++ lesson-8/exercises/2.js
@@ -1,12 +1,32 @@
 /**
  * Write your own version of Array.reduce that receives
- * 
+ *
  * - An array
  * - A function
  * - The initial value of the result
- * 
+ *
  * Example: myReduce([1, 2, 3], function (total, item) {return total + item}, 0)
  * returns 6
- * 
+ *
  * Use this function to multiply all the items in an array
  */
+
+ function myReduce(array, func, initialVal) {
+    let result = initialVal;
+    for (let counter = 0; counter !== array.length; counter++) {
+      const item = array.at(counter);
+      result = func(result, item);
+    }
+    return result;
+  }
+  
+  console.log(
+    myReduce(
+      [4, 5, 4],
+      function (result, item) {
+        return result * item;
+      },
+      1
+    )
+  );
+  
