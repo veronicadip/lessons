cf7043e4bf74d9a72f0955c22d23efd0ac3849a7
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-02-24 11:32:54 -0300

    js1 checked (#23)
    js1 done

--- lesson-8/exercises/1.js
+++ lesson-8/exercises/1.js
@@ -1,7 +1,27 @@
 /**
  * Write your own version of Array.map that receives an
  * array and a function and applies the function to each item
- * 
+ *
  *  example: myMap([1, 2, 3], function (item) { return item + 1 })
  *  returns [2, 3, 4]
  */
+
+function myMap(array, func) {
+  const result = [];
+  for (let counter = 0; counter !== array.length; counter++) {
+    const item = array.at(counter);
+    result.push(func(item));
+  }
+  return result;
+}
+
+console.log(
+  myMap([1, 2, 3], function (item) {
+    return item + 1;
+  })
+);
+console.log(
+  myMap([1, 2, 3], function (item) {
+    return item * 10;
+  })
+);
