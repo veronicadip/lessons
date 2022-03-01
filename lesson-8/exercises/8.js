4850bdca941b89f47339e8147d62b6b49016de98
Vero <vero@Robertos-Mac-mini-2.local>
2022-02-28 23:49:49 -0300

    js8 ready to be checked

--- lesson-8/exercises/8.js
+++ lesson-8/exercises/8.js
@@ -3,6 +3,10 @@
  * to write a function that passes the tests
  */
 
+const pukiFunc = (currentString) => currentString === "puki"
+console.log(allPukis.every(pukiFunc))
+
+
 if (allPukis(["puki", "puki", "puki"]) !== true) {
   throw new Error(
     "it should return true when all the items in the array are pukis"
