f294c9a31383364e8df889c31b3a9be8fe3c2d1e
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-03-02 15:27:07 -0300

    js4 checked (#26)
    js4 already checked

--- lesson-8/exercises/4.js
+++ lesson-8/exercises/4.js
@@ -5,3 +5,58 @@
  * classes would you create and what attributes could they have?
  *
  */
+
+
+class Person {
+    constructor(personName, personAge, personLesson) {
+        this.name = personName;
+        this.age = personAge;
+        this.lesson = personLesson
+    }
+}
+
+class Professor extends Person {
+    constructor(personName, personAge, personLesson) {
+        super(personName, personAge, personLesson)
+    }
+    present() {
+        console.log("Hi, my name is " + this.name + ", I'm " + this.age + " years old and I will teach you " + this.lesson);
+    }
+}
+
+const fulano = new Professor("Fulano", 37, "history");
+fulano.present();
+
+const mengano = new Professor("Mengano", 49, "english");
+mengano.present();
+
+class Student extends Person {
+    constructor(personName, personAge, studentYear, personLesson) {
+        super(personName, personAge, personLesson)
+        this.year = studentYear
+    }
+    present() {
+        console.log("Hi, my name is " + this.name + ", I'm " + this.age + " years old and I'm at my " + this.year + " year in this school");
+    }
+    studLessons() {
+        console.log("I'm taking these classes: " + this.lesson.join(", "))
+    }
+}
+
+const fulanito = new Student("Fulanito", 14, "third", ["history", "english", "psicology"]);
+fulanito.present();
+fulanito.studLessons();
+
+
+const menganito = new Student("Menganito", 16, "fifth", ["spanish", "math", "biology", "history II"]);
+menganito.present();
+menganito.studLessons();
+
+
+
+/**
+ * Hey, I'm {pesonName} and I'm taking the following lessons:
+ * 
+ * - {lesson 1}
+ * - {lesson 2}
+ */
