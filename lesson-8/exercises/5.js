e58118efffb4b98d8cac6a9ee68e53d9156286b1
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-03-02 15:27:35 -0300

    js5 ready to be checked (#27)
    * js5 ready to be checked
    
    * js5 corrections
    
    Co-authored-by: Vero <vero@Robertos-Mac-mini-2.local>

--- lesson-8/exercises/5.js
+++ lesson-8/exercises/5.js
@@ -7,3 +7,21 @@
  * - A method `isSquare` that returns a boolean indicating if all sides are equal
  *
  */
+
+class Rectangle {
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+    }
+    area() {
+        console.log(this.width * this.height)
+    }
+    isSquare() {
+        return this.width === this.height
+    }
+}
+
+
+const rectangle1 = new Rectangle(10, 15);
+rectangle1.area()
+rectangle1.isSquare()
