89a64ed8aa767d88803a26f364b4200c99795a22
Roberto Dip <dip.jesusr@gmail.com>
2022-03-14 10:59:00 -0300

    lesson 10 exercises

---
+++ lesson-10/exercises/1.js
@@ -0,0 +1,31 @@
+// Convert the following functions to arrow functions
+
+function f1() {
+  console.log("f1");
+}
+
+function f2(a, b, c) {
+  return a + b + c;
+}
+
+setInterval(function () {
+  console.log("interval passed");
+}, 1000);
+
+[1, 2, 3, 4].map(function (item, index) {
+  const temp = item + index;
+  return temp * item;
+});
+
+function f3(n) {
+  return function () {
+    return n + 1;
+  };
+}
+
+const f4 = function (a, b) {
+  return Math.pow(a, b)
+}
+
+// bonus, try to understand what this does and what's the result
+[1, 2, 3].reduce(f4)
