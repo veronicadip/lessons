11d78b8ddb4e3f513ec21fc324457436614cbb35
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-03-15 15:05:36 -0300

    l10e1 checked (#44)
    e1 done

--- lesson-10/exercises/1.js
+++ lesson-10/exercises/1.js
@@ -1,31 +1,32 @@
 // Convert the following functions to arrow functions
 
-function f1() {
-  console.log("f1");
-}
+const f1 = () => console.log("f1");
 
-function f2(a, b, c) {
-  return a + b + c;
-}
 
-setInterval(function () {
-  console.log("interval passed");
-}, 1000);
+const f2 = (a, b, c) => a + b + c;
 
-[1, 2, 3, 4].map(function (item, index) {
+
+setInterval(() => console.log("interval passed"), 1000);
+
+[1, 2, 3, 4].map((item, index) => {
   const temp = item + index;
   return temp * item;
 });
 
-function f3(n) {
-  return function () {
-    return n + 1;
-  };
-}
+const f3 = (n) => () => n + 1
 
-const f4 = function (a, b) {
-  return Math.pow(a, b);
-};
+const f4 = (a, b) => Math.pow(a, b)
+
 
 // bonus, try to understand what this does and what's the result
-[1, 2, 3].reduce(f4);
+[1, 2, 3].reduce(f4)
+
+/** 1- .reduce takes the two first numbers on the array and calls the
+ * f4 function.
+ * 2- The first number (a) is used as the base number and the second
+ * one (b) is the exponent used to raise the base.
+ * 3- On this case the result is 1 (1² = 1).
+ * 4- Now .reduce will take the result as the new value for (a), and the
+ * next number on the array (in this case 3) as the value for (b).
+ * 5- The final result is 1 because 1³ = 1
+*/
