3c13b5a4183fd96449fa3b95531eee3d5e302590
veronicadip <dipmveronica@gmail.com>
2022-03-15 17:46:24 -0300

    e4 still in process

--- lesson-10/exercises/4.js
+++ lesson-10/exercises/4.js
@@ -13,17 +13,47 @@
 // `print('abc', {letterSpacing: 2, upper: true})` prints in the console the string 'A  B  C'
 
 
-function print(str, obj) {
-    if (obj.upper === true) {
-        console.log(str.toUpperCase())
-    }
+function print1(str, obj) {
+    result = ''
+    finalResult = []
     for (let counter = 0; counter !== obj.repeat; counter++) {
-        console.log(str)
+        result = str
+        if (obj.upper === true) {
+            result = result.toUpperCase()
+            for (let count = 0; count !== obj.letterSpacing; count++) {
+                for (let counting = 0; counting !== str.length; counting++) {
+                    finalResult = finalResult.push(result.at(counting) + ' ')
+                    console.log(result)
+                }
+            }
+        }
     }
-    for (let count = 0; count !== obj.letterSpacing; count++) {
-        console.log(str.join(' '))
-    }
-
 }
 
-print('abc', {upper: true, repeat: 4})
+function print3(str, obj) {
+    result = ''
+    finalResult = ''
+    if (obj.upper === true) {
+        result = str.toUpperCase()
+        for (let counter1 = 0; counter1 <= obj.letterSpacing; counter1++) {
+            for (let counter2 = 0; counter2 !== result.length; counter2++) {
+                finalResult = finalResult + result.at(counter2) + ' '
+            } console.log(finalResult)
+        }
+    }
+}
+
+function print2(str, obj) {
+    result = ''
+    for (let counter = 0; counter !== obj.repeat; counter++) {
+        result = str
+        for (let count = 0; count <= obj.letterSpacing; count++) {
+            for (let c = 0; c !== result.length; c++) {
+                result = result.at(c) + ' '
+                console.log(result)
+            }
+        }
+    }
+}
+
+print3('abc', {upper: true, repeat: 2, letterSpacing: 1})
