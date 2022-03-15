543e33cc9f279cc2a235f2700f9442b6d215f30a
veronicadip <dipmveronica@gmail.com>
2022-03-15 12:55:49 -0300

    e3 ready to be checked

--- lesson-10/exercises/3.js
+++ lesson-10/exercises/3.js
@@ -3,3 +3,17 @@
 //
 // Once you have explained read https://eloquentjavascript.net/03_functions.html
 // and try to explain it again
+
+/** a function is a piece of code that you can write in different ways. Once you have it, you can call
+ * it any time and it will do different things depending on what you coded. For example
+ * you can make booleans, you can make it return the thing that you need (it can be a simple
+ * string or even another piece of code), you can make a loop, or even call another function.
+ * you can also write an arrow function; it is a shorter way to do it but it have some differences.
+ */
+
+/**new definition: a function is a piece of code that you can write to call it later, this way you can avoid
+ * repeating the same thing multiple times.
+ * functions can receive multiple parameters (but you can run a function without one parameter at all) and have a
+ * piece of code that is executed every time you call the function (using the parameters you give to it on
+ * each case). The return statement determines what is the function returning after all the process it made.
+ */
