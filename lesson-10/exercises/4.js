84c6c85540f2d99d433bcf9028b0017e11ca5a00
veronicadip <dipmveronica@gmail.com>
2022-03-16 13:21:08 -0300

    e4 ready to be checked

--- lesson-10/exercises/4.js
+++ lesson-10/exercises/4.js
@@ -12,48 +12,22 @@
 // Example:
 // `print('abc', {letterSpacing: 2, upper: true})` prints in the console the string 'A  B  C'
 
-
-function print1(str, obj) {
-    result = ''
-    finalResult = []
-    for (let counter = 0; counter !== obj.repeat; counter++) {
-        result = str
-        if (obj.upper === true) {
-            result = result.toUpperCase()
-            for (let count = 0; count !== obj.letterSpacing; count++) {
-                for (let counting = 0; counting !== str.length; counting++) {
-                    finalResult = finalResult.push(result.at(counting) + ' ')
-                    console.log(result)
-                }
-            }
-        }
+function print(str, obj) {
+  result = "";
+  finalResult = "";
+  space = " ";
+  if (obj.upper === true) {
+    result = str.toUpperCase();
+    for (let counter1 = 0; counter1 !== result.length; counter1++) {
+      finalResult = `${finalResult}${result.at(counter1)}${space.repeat(
+        obj.letterSpacing
+      )}`;
     }
+  }
+  if (obj.repeat >= 0) {
+    finalResult = `${finalResult.repeat(obj.repeat)}`;
+  }
+  console.log(finalResult);
 }
 
-function print3(str, obj) {
-    result = ''
-    finalResult = ''
-    if (obj.upper === true) {
-        result = str.toUpperCase()
-        for (let counter1 = 0; counter1 <= obj.letterSpacing; counter1++) {
-            for (let counter2 = 0; counter2 !== result.length; counter2++) {
-                finalResult = finalResult + result.at(counter2) + ' '
-            } console.log(finalResult)
-        }
-    }
-}
-
-function print2(str, obj) {
-    result = ''
-    for (let counter = 0; counter !== obj.repeat; counter++) {
-        result = str
-        for (let count = 0; count <= obj.letterSpacing; count++) {
-            for (let c = 0; c !== result.length; c++) {
-                result = result.at(c) + ' '
-                console.log(result)
-            }
-        }
-    }
-}
-
-print3('abc', {upper: true, repeat: 2, letterSpacing: 1})
+print("abc", { upper: true, letterSpacing: 3, repeat: 2 });
