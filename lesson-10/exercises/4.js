4047c0a766b780bd20c86046d1a115b105311a30
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-03-17 10:20:14 -0300

    l10e4 checked (#47)
    e4 done

--- lesson-10/exercises/4.js
+++ lesson-10/exercises/4.js
@@ -11,3 +11,24 @@
 //
 // Example:
 // `print('abc', {letterSpacing: 2, upper: true})` prints in the console the string 'A  B  C'
+
+function print(str, obj = {upper: false, repeat: 1, letterSpacing: 0}) {
+  let result = str;
+  let finalResult = "";
+  const space = " ";
+
+  if (obj.upper === true) {
+    result = str.toUpperCase();
+  } 
+
+  for (let letter of result) {
+    finalResult = `${finalResult}${letter}${space.repeat(obj.letterSpacing)}`
+  }
+
+  if (obj.repeat >= 0) {
+    finalResult = `${finalResult.repeat(obj.repeat)}`;
+  }
+  console.log(finalResult);
+}
+
+print("abc", {upper: true, repeat: 3, letterSpacing: 5});
