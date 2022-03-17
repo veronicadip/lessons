3517d114cdaabf3f84bf36eea6a4d1a63509f852
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-03-17 10:23:37 -0300

    e6 checked (#49)
    e6 done

--- lesson-10/exercises/6.js
+++ lesson-10/exercises/6.js
@@ -1,1 +1,17 @@
 // Define a function that receives an object and prints every value in the object
+
+function objVal(obj) {
+    return Object.values(obj)
+}
+
+function print(obj, func) {
+    let result = ''
+    const arr = objVal(obj)
+    for (let counter = 0; counter !== arr.length; counter++) {
+        result = arr.at(counter)
+        console.log(result)
+    } 
+}
+
+
+print({name: "puki", age: 0})
