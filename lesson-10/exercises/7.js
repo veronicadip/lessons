e02eb4bcaa7475a302c31fbc2b100e8adc3f8c38
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-03-25 11:15:31 -0300

    e7 checked (#50)
    e7 done

--- lesson-10/exercises/7.js
+++ lesson-10/exercises/7.js
@@ -7,5 +7,13 @@
 // 2. think about what setState does and try to mimic that
 
 class Component {
-  // TODO: implement the function setState
+  constructor(props) {
+    this.props = props;
+    this.state = {};
+  }
+
+  setState(newState) {
+    const newObj = Object.assign(this.state, newState);
+    return render();
+  }
 }
